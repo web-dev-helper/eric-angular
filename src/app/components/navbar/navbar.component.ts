4f1866a14e6eaa9da9c307f7d30cf49c74f23912
Eric Sim <eric.sim@scotiabank.com>
2017-11-02 17:53:18 -0400

    Implementing database read/write

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  loginInd: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
